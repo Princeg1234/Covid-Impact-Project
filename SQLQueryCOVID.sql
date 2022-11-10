ea05e6a30d6c79abd1dec8078f88a8f2d4c27dd9
Princeg1234 <117936967+Princeg1234@users.noreply.github.com>
2022-11-10 13:11:53 -0500

    Add files via upload

---
+++ SQLQueryCOVID.sql
@@ -0,0 +1,3 @@
+Select state, date, all_pediatric_inpatient_bed_occupied, all_pediatric_inpatient_bed_occupied_coverage
+From dbo.[COVID-19_Reported_Patient_Impact_and_Hospital_Capacity_by_State_Timeseries]
+Where state = 'MN' AND date = '2021/11/11'
